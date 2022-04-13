5c7dbb1b8ac9e00c9197e307df420b19359c2ea5
roberth26 <robbiehall26@gmail.com>
2022-04-12 20:26:59 -0700

    use staticPath in all places

--- components/Head.jsx
+++ components/Head.jsx
@@ -6,11 +6,7 @@
     content="Far and Wide Studios is a fully-remote animation cohort that focuses on stories and 3D animation."
   />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link
-    rel="icon"
-    type="image/png"
-    href={join(staticPath, '/images/favicon.png')}
-  />
+  <link rel="icon" type="image/png" href={staticAsset('/images/favicon.png')} />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
   <link
@@ -21,15 +17,15 @@
     rel="stylesheet"
     href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap"
   />
-  <link rel="stylesheet" href={join(staticPath, '/reset.css')} />
+  <link rel="stylesheet" href={staticAsset('/reset.css')} />
   <link
     rel="stylesheet"
-    href={join(staticPath, './fontawesome/css/fontawesome.min.css')}
+    href={staticAsset('./fontawesome/css/fontawesome.min.css')}
   />
   <link
     rel="stylesheet"
-    href={join(staticPath, './fontawesome/css/brands.min.css')}
+    href={staticAsset('./fontawesome/css/brands.min.css')}
   />
-  <link rel="stylesheet" href={join(staticPath, '/styles.css')} />
+  <link rel="stylesheet" href={staticAsset('/styles.css')} />
   {children}
 </head>;
